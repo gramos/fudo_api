768f3fef3347141fe1f6f71002b29829960a457e
Gaston Ramos <ramos.gaston@gmail.com>
2026-09-14 15:31:25 -0300

    Agrego mas tests de integracion para /products

--- test/integration/config_test.rb
+++ test/integration/config_test.rb
@@ -3,6 +3,42 @@ require "rack"
 require_relative "../test_helper"
 
 class ConfigTest < Minitest::Test
+  def test_authenticated_client_can_list_products
+    ENV["AUTH_USERNAME"] = "admin"
+    ENV["AUTH_PASSWORD"] = "secret"
+    app, = Rack::Builder.parse_file("config.ru")
+    auth_env = Rack::MockRequest.env_for(
+      "/auth",
+      method: "POST",
+      input: JSON.generate(username: "admin", password: "secret"),
+      "CONTENT_TYPE" => "application/json"
+    )
+    _status, _headers, auth_body = app.call(auth_env)
+    token = JSON.parse(auth_body.join)["token"]
+    list_env = Rack::MockRequest.env_for(
+      "/products",
+      method: "GET",
+      "HTTP_AUTHORIZATION" => "Bearer #{token}"
+    )
+
+    status, _headers, body = app.call(list_env)
+
+    assert_equal 200, status
+    assert_equal [], JSON.parse(body.join)
+  end
+
+  def test_unauthenticated_client_cannot_list_products
+    ENV["AUTH_USERNAME"] = "admin"
+    ENV["AUTH_PASSWORD"] = "secret"
+    app, = Rack::Builder.parse_file("config.ru")
+    env = Rack::MockRequest.env_for("/products", method: "GET")
+
+    status, _headers, body = app.call(env)
+
+    assert_equal 401, status
+    assert_equal({ "error" => "Unauthorized" }, JSON.parse(body.join))
+  end
+
   def test_unauthenticated_client_cannot_create_a_product
     ENV["AUTH_USERNAME"] = "admin"
     ENV["AUTH_PASSWORD"] = "secret"
