20a4b09fce9e00406f80bb855b28170e513b09c9
Gaston Ramos <ramos.gaston@gmail.com>
2026-09-14 15:09:12 -0300

    Agrego test para /auth como ruta publica

--- test/auth_middleware_test.rb
+++ test/auth_middleware_test.rb
@@ -3,6 +3,17 @@ require_relative "../auth"
 require_relative "../auth_middleware"
 
 class AuthMiddlewareTest < Minitest::Test
+  def test_public_route_does_not_require_a_token
+    next_app = lambda { |_env| [200, {}, ["public"]] }
+    middleware = AuthMiddleware.new(next_app, auth: nil)
+    env = { "PATH_INFO" => "/auth" }
+
+    status, _headers, body = middleware.call(env)
+
+    assert_equal 200, status
+    assert_equal ["public"], body
+  end
+
   def test_missing_token_is_rejected
     auth = Auth.new(username: "admin", password: "secret")
     next_app = lambda { |_env| flunk "The request should not reach the next app" }
